ee94d2bd57370604aaaee2fbc1ed88b1451943aa
redphx <96280+redphx@users.noreply.github.com>
2023-07-23 15:25:13 +0700

    Add button to toggle Stream Stats

--- better-xcloud.user.js
+++ better-xcloud.user.js
@@ -74,7 +74,10 @@ class StreamStatus {
 
 
 class StreamStats {
+    static #timeout;
     static #updateInterval = 1000;
+
+    static #$container;
     static #$fps;
     static #$rtt;
     static #$pl;
@@ -83,11 +86,34 @@ class StreamStats {
 
     static #lastInbound;
 
+    static start() {
+        StreamStats.#$container.style.display = 'block';
+        StreamStats.update();
+    }
+
+    static stop() {
+        StreamStats.#$container.style.display = 'none';
+        clearTimeout(StreamStats.#timeout);
+        StreamStats.#timeout = null;
+        StreamStats.#lastInbound = null;
+    }
+
+    static toggle() {
+        StreamStats.#isHidden() ? StreamStats.start() : StreamStats.stop();
+    }
+
+    static #isHidden = () => StreamStats.#$container.style.display === 'none';
+
     static update() {
-        if (!STREAM_WEBRTC) {
-            setTimeout(StreamStats.update, StreamStats.#updateInterval);
+        if (StreamStats.#isHidden()) {
             return;
         }
+
+        if (!STREAM_WEBRTC) {
+            StreamStats.#timeout = setTimeout(StreamStats.update, StreamStats.#updateInterval);
+            return;
+        }
+
         STREAM_WEBRTC.getStats().then(stats => {
             stats.forEach(stat => {
                 if (stat.type === 'inbound-rtp' && stat.kind === 'video') {
@@ -118,13 +144,17 @@ class StreamStats {
                 }
             });
 
-            setTimeout(StreamStats.update, StreamStats.#updateInterval);
+            StreamStats.#timeout = setTimeout(StreamStats.update, StreamStats.#updateInterval);
         });
     }
 
     static render() {
+        if (StreamStats.#$container) {
+            return;
+        }
+
         const CE = createElement;
-        const $wrapper = CE('div', {'class': 'better_xcloud_stats_bar'},
+        StreamStats.#$container = CE('div', {'class': 'better_xcloud_stats_bar'},
                             CE('label', {}, 'FPS'),
                             StreamStats.#$fps = CE('span', {}, 0),
                             CE('label', {}, 'RTT'),
@@ -136,12 +166,9 @@ class StreamStats {
                             CE('label', {}, 'FL'),
                             StreamStats.#$fl = CE('span', {}, '0 (0.00%)'));
 
-        return $wrapper;
+        document.documentElement.appendChild(StreamStats.#$container);
     }
 }
-
-document.documentElement.appendChild(StreamStats.render());
-StreamStats.update();
 
 
 class Preferences {
@@ -475,6 +502,7 @@ div[class*=StreamMenu-module__menuContainer] > div[class*=Menu-module] {
 }
 
 .better_xcloud_stats_bar {
+    display: none;
     position: absolute;
     top: 0;
     left: 0;
@@ -1033,6 +1061,19 @@ function watchHeader() {
 }
 
 
+function cloneStreamMenuButton($orgButton, label, svg_icon) {
+    const $button = $orgButton.cloneNode(true);
+    $button.setAttribute('aria-label', label);
+    $button.querySelector('div[class*=label]').textContent = label;
+
+    const $svg = $button.querySelector('svg');
+    $svg.innerHTML = svg_icon;
+    $svg.setAttribute('viewBox', '0 0 24 24');
+
+    return $button;
+}
+
+
 function injectVideoSettingsButton() {
     const $screen = document.querySelector('#PageContent section[class*=PureScreens]');
     if (!$screen) {
@@ -1074,29 +1115,16 @@ function injectVideoSettingsButton() {
                     return;
                 }
 
-                const id = 'better-xcloud-video-settings-btn';
-                let $wrapper = document.getElementById('#' + id);
-                if ($wrapper) {
-                    return;
-                }
-
                 const $orgButton = node.querySelector('div > div > button');
                 if (!$orgButton) {
                     return;
                 }
 
-                // Clone other button
-                const $button = $orgButton.cloneNode(true);
-                $button.setAttribute('aria-label', 'Video settings');
-                $button.querySelector('div[class*=label]').textContent = 'Video settings';
-
                 // Credit: https://www.iconfinder.com/iconsets/user-interface-outline-27
-                const SVG_ICON = '<path d="M8 2c-1.293 0-2.395.843-2.812 2H3a1 1 0 1 0 0 2h2.186C5.602 7.158 6.706 8 8 8s2.395-.843 2.813-2h10.188a1 1 0 1 0 0-2H10.813C10.395 2.843 9.293 2 8 2zm0 2c.564 0 1 .436 1 1s-.436 1-1 1-1-.436-1-1 .436-1 1-1zm7 5c-1.293 0-2.395.843-2.812 2H3a1 1 0 1 0 0 2h9.186c.417 1.158 1.521 2 2.814 2s2.395-.843 2.813-2H21a1 1 0 1 0 0-2h-3.187c-.418-1.157-1.52-2-2.813-2zm0 2c.564 0 1 .436 1 1s-.436 1-1 1-1-.436-1-1 .436-1 1-1zm-7 5c-1.293 0-2.395.843-2.812 2H3a1 1 0 1 0 0 2h2.188c.417 1.157 1.519 2 2.813 2s2.398-.842 2.814-2H21a1 1 0 1 0 0-2H10.812c-.417-1.157-1.519-2-2.812-2zm0 2c.564 0 1 .436 1 1s-.436 1-1 1-1-.436-1-1 .436-1 1-1z"/>';
-                const $svg = $button.querySelector('svg');
-                $svg.innerHTML = SVG_ICON;
-                $svg.setAttribute('viewBox', '0 0 24 24');
-
-                $button.addEventListener('click', e => {
+                const ICON_VIDEO_SETTINGS = '<path d="M8 2c-1.293 0-2.395.843-2.812 2H3a1 1 0 1 0 0 2h2.186C5.602 7.158 6.706 8 8 8s2.395-.843 2.813-2h10.188a1 1 0 1 0 0-2H10.813C10.395 2.843 9.293 2 8 2zm0 2c.564 0 1 .436 1 1s-.436 1-1 1-1-.436-1-1 .436-1 1-1zm7 5c-1.293 0-2.395.843-2.812 2H3a1 1 0 1 0 0 2h9.186c.417 1.158 1.521 2 2.814 2s2.395-.843 2.813-2H21a1 1 0 1 0 0-2h-3.187c-.418-1.157-1.52-2-2.813-2zm0 2c.564 0 1 .436 1 1s-.436 1-1 1-1-.436-1-1 .436-1 1-1zm-7 5c-1.293 0-2.395.843-2.812 2H3a1 1 0 1 0 0 2h2.188c.417 1.157 1.519 2 2.813 2s2.398-.842 2.814-2H21a1 1 0 1 0 0-2H10.812c-.417-1.157-1.519-2-2.812-2zm0 2c.564 0 1 .436 1 1s-.436 1-1 1-1-.436-1-1 .436-1 1-1z"/>';
+                // Create Video Settings button
+                const $btnVideoSettings = cloneStreamMenuButton($orgButton, 'Video settings', ICON_VIDEO_SETTINGS);
+                $btnVideoSettings.addEventListener('click', e => {
                     e.preventDefault();
                     e.stopPropagation();
 
@@ -1107,23 +1135,38 @@ function injectVideoSettingsButton() {
                     $parent.addEventListener('touchend', hideQuickBarFunc);
 
                     const $touchSurface = document.querySelector('#MultiTouchSurface');
-                    if ($touchSurface) {
-                        $touchSurface.addEventListener('touchstart', hideQuickBarFunc);
-                    }
+                    $touchSurface && $touchSurface.addEventListener('touchstart', hideQuickBarFunc);
                 });
 
-                $orgButton.parentElement.insertBefore($button, $orgButton.parentElement.firstChild);
+                // Add button at the beginning
+                $orgButton.parentElement.insertBefore($btnVideoSettings, $orgButton.parentElement.firstChild);
 
                 // Hide Quick bar when closing HUD
-                document.querySelector('button[class*=StreamMenu-module__backButton]').addEventListener('click', e => {
+                const $btnCloseHud = document.querySelector('button[class*=StreamMenu-module__backButton]');
+                $btnCloseHud.addEventListener('click', e => {
                     $quickBar.style.display = 'none';
                 });
+
+                const ICON_STREAM_STATS = '<path d="M12.005 5C9.184 5 6.749 6.416 5.009 7.903c-.87.743-1.571 1.51-2.074 2.18-.251.335-.452.644-.605.934-.434.733-.389 1.314-.004 1.98a6.98 6.98 0 0 0 .609.949 13.62 13.62 0 0 0 2.076 2.182C6.753 17.606 9.188 19 12.005 19s5.252-1.394 6.994-2.873a13.62 13.62 0 0 0 2.076-2.182 6.98 6.98 0 0 0 .609-.949c.425-.737.364-1.343-.004-1.98-.154-.29-.354-.599-.605-.934-.503-.669-1.204-1.436-2.074-2.18C17.261 6.416 14.826 5 12.005 5zm0 2c2.135 0 4.189 1.135 5.697 2.424.754.644 1.368 1.32 1.773 1.859.203.27.354.509.351.733s-.151.462-.353.732c-.404.541-1.016 1.214-1.77 1.854C16.198 15.881 14.145 17 12.005 17s-4.193-1.12-5.699-2.398a11.8 11.8 0 0 1-1.77-1.854c-.202-.27-.351-.508-.353-.732s.149-.463.351-.733c.406-.54 1.019-1.215 1.773-1.859C7.816 8.135 9.87 7 12.005 7zm.025 1.975c-1.645 0-3 1.355-3 3s1.355 3 3 3 3-1.355 3-3-1.355-3-3-3zm0 2c.564 0 1 .436 1 1s-.436 1-1 1-1-.436-1-1 .436-1 1-1z"/>';
+                // Create Stream Stats button
+                const $btnStreamStats = cloneStreamMenuButton($orgButton, 'Stream stats', ICON_STREAM_STATS);
+                $btnStreamStats.addEventListener('click', e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    // Close HUD
+                    $btnCloseHud.click();
+                    // Toggle Stream Stats
+                    StreamStats.toggle();
+                });
+
+                // Insert after Video Settings button
+                $orgButton.parentElement.insertBefore($btnStreamStats, $btnVideoSettings.nextSibling);
 
                 // Render stream badges
                 const $menu = document.querySelector('div[class*=StreamMenu-module__menuContainer] > div[class*=Menu-module]');
                 $menu.appendChild(StreamStatus.render());
             });
-
         });
     });
     observer.observe($screen, {subtree: true, childList: true});
@@ -1514,6 +1557,7 @@ function hideUiOnPageChange() {
 
     STREAM_WEBRTC = null;
     $STREAM_VIDEO = null;
+    StreamStats.stop();
     document.querySelector('.better_xcloud_screenshot_button').style = '';
 }
 
@@ -1544,6 +1588,7 @@ addCss();
 updateVideoPlayerCss();
 setupVideoSettingsBar();
 setupScreenshotButton();
+StreamStats.render();
 
 // Workaround for Hermit browser
 var onLoadTriggered = false;
