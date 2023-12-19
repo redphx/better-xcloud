0870065a81c5b8c8d22078c9e15eb3cd65b24d33
redphx <96280+redphx@users.noreply.github.com>
2023-12-19 18:10:50 +0700

    Bump version to 2.1.2

--- better-xcloud.meta.js
+++ better-xcloud.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @name         Better xCloud
 // @namespace    https://github.com/redphx
-// @version      2.1.1
+// @version      2.1.2
 // ==/UserScript==
--- better-xcloud.user.js
+++ better-xcloud.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better xCloud
 // @namespace    https://github.com/redphx
-// @version      2.1.1
+// @version      2.1.2
 // @description  Improve Xbox Cloud Gaming (xCloud) experience
 // @author       redphx
 // @license      MIT
@@ -13,7 +13,7 @@
 // ==/UserScript==
 'use strict';
 
-const SCRIPT_VERSION = '2.1.1';
+const SCRIPT_VERSION = '2.1.2';
 const SCRIPT_HOME = 'https://github.com/redphx/better-xcloud';
 
 const ENABLE_MKB = false;
