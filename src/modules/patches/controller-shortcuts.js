030791d9c4ff406cf28c3c21f140a33fba4e76ac
redphx <96280+redphx@users.noreply.github.com>
2024-05-26 10:54:32 +0700

    Format

--- src/modules/patches/controller-shortcuts.js
+++ src/modules/patches/controller-shortcuts.js
@@ -29,9 +29,9 @@ if (btnHome) {
             // First time pressing > save current timestamp
             window.BX_EXPOSED.resetControllerShortcut(currentGamepad.index);
             this.bxHomeStates[currentGamepad.index] = {
-                    shortcutPressed: 0,
-                    timestamp: currentGamepad.timestamp,
-                };
+                shortcutPressed: 0,
+                timestamp: currentGamepad.timestamp,
+            };
         }
 
         // Listen to next button press
@@ -48,33 +48,33 @@ if (btnHome) {
 
         if (info.shortcutPressed === 0) {
             const fakeGamepadMappings = [{
-                    GamepadIndex: currentGamepad.index,
-                    A: 0,
-                    B: 0,
-                    X: 0,
-                    Y: 0,
-                    LeftShoulder: 0,
-                    RightShoulder: 0,
-                    LeftTrigger: 0,
-                    RightTrigger: 0,
-                    View: 0,
-                    Menu: 0,
-                    LeftThumb: 0,
-                    RightThumb: 0,
-                    DPadUp: 0,
-                    DPadDown: 0,
-                    DPadLeft: 0,
-                    DPadRight: 0,
-                    Nexus: 1,
-                    LeftThumbXAxis: 0,
-                    LeftThumbYAxis: 0,
-                    RightThumbXAxis: 0,
-                    RightThumbYAxis: 0,
-                    PhysicalPhysicality: 0,
-                    VirtualPhysicality: 0,
-                    Dirty: true,
-                    Virtual: false,
-                }];
+                GamepadIndex: currentGamepad.index,
+                A: 0,
+                B: 0,
+                X: 0,
+                Y: 0,
+                LeftShoulder: 0,
+                RightShoulder: 0,
+                LeftTrigger: 0,
+                RightTrigger: 0,
+                View: 0,
+                Menu: 0,
+                LeftThumb: 0,
+                RightThumb: 0,
+                DPadUp: 0,
+                DPadDown: 0,
+                DPadLeft: 0,
+                DPadRight: 0,
+                Nexus: 1,
+                LeftThumbXAxis: 0,
+                LeftThumbYAxis: 0,
+                RightThumbXAxis: 0,
+                RightThumbYAxis: 0,
+                PhysicalPhysicality: 0,
+                VirtualPhysicality: 0,
+                Dirty: true,
+                Virtual: false,
+            }];
 
             const isLongPress = (currentGamepad.timestamp - info.timestamp) >= 500;
             const intervalMs = isLongPress ? 500 : 100;
