7aee4d51484cab4d7ad5b84b4bd636ad280ea1e4
redphx <96280+redphx@users.noreply.github.com>
2024-07-01 17:20:39 +0700

    Compress CSS

--- src/macros/build.ts
+++ src/macros/build.ts
@@ -4,6 +4,7 @@ import cssStr from "@assets/css/styles.styl" with { type: "text" };
 
 const generatedCss = await (stylus(cssStr, {})
 	.set('filename', 'styles.css')
+	.set('compress', true)
 	.include('src/assets/css/'))
 	.render();
 
